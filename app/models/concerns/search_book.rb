9529889765117f58f7c0e51c7d08be4e88b9dc44
SuguruTAKA <bo9iro@gmail.com>
2018-04-12 08:15:04 +0000

    kaminari

---
+++ app/models/concerns/search_book.rb
@@ -0,0 +1,26 @@
+module SearchBook
+	extend ActiveSupport::Concern
+	included do
+		scope :search_title, lambda { |keyword|
+			table = Book.arel_table
+			condition = table[:title].matches("%#{keyword}%")
+			where(condition)
+		}
+		scope :search_author, lambda { |keyword|
+			table = Book.arel_table
+			condition = table[:author].matches("%#{keyword}%")
+			where(condition)
+		}
+
+		scope :search, lambda { |s|
+			r = self
+			r = r.search_title(s[:s_title]) if s[:s_title].present?
+			r = r.search_author(s[:s_author]) if s[:s_author].present?
+			if r != self
+				r
+			else
+				where({})
+			end
+		}
+	end
+end
